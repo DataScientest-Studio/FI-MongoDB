1e14a72946782b3fc82dbf67316310fc3f958f4d
Skypeblue <fredericfrancine@gmail.com>
2022-10-19 12:04:15 +0200

    201

---
+++ 201/setup.sh
@@ -0,0 +1,7 @@
+mkdir data 
+
+cd data 
+
+wget https://dst-de.s3.eu-west-3.amazonaws.com/mongo_fr/companies.json
+wget https://dst-de.s3.eu-west-3.amazonaws.com/mongo_fr/grades.json
+wget https://dst-de.s3.eu-west-3.amazonaws.com/mongo_fr/zips.json
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # FI-MongoDB
+
+Le but de ce repo Git est de retrouver les commandes utilisées lors des différentes formations intra sur MongoDB.
+
